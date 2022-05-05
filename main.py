9542b5af5a26183a1a2830af7b86b7c910a51e83
corbin <stramapatryk@gmail.com>
2022-05-05 21:05:09 +0200

    Made int_input function that get correct int from terminal input. Completed counter function. Completed timer function. Completed clock function. Completed get_time function. Completed alarm function

--- main.py
+++ main.py
@@ -1,20 +1,61 @@
 import time
 
 
+def int_input(text="Enter integer: "):
+    while True:
+        print(text, end="")
+        value = input()
+        if value.isdecimal():
+            return int(value)
+        else:
+            print("Wrong input")
+
+
 def counter():
-    pass
+    max_value = int_input("Enter number to count to: ")
+
+    for i in range(0, max_value):
+        print(i)
+        time.sleep(1)
 
 
 def timer():
-    pass
+    remaining_seconds = int_input("Enter how many seconds to wait: ")
+
+    while remaining_seconds != 0:
+        print(remaining_seconds, "s", sep="")
+        remaining_seconds -= 1
+        time.sleep(1)
+
+    print("It's ended")
 
 
 def clock():
-    pass
+    print(time.strftime("%a, %d %b %Y %H:%M:%S", time.localtime()))
+
+
+def get_time():
+    time_result = []
+    hours = int_input("Enter a hour: ")
+    minute = int_input("Enter a minute: ")
+    return [hours, minute]
 
 
 def alarm():
-    pass
+    alarm_time = get_time()
+    time_now = time.localtime()
+
+    get_seconds = (alarm_time[0] - time_now.tm_hour) * 360
+    get_seconds += (alarm_time[1] - time_now.tm_min) * 60
+    get_seconds -= time_now.tm_sec
+
+    if get_seconds <= 0:
+        print("Wrong time")
+        return
+
+    print("alarm start now.", get_seconds, "seconds to wait")
+    time.sleep(get_seconds)
+    print("ring ring, it's the alarm")
 
 
 input_program = {"c": counter, "t": timer, "C": clock, "a": alarm}
@@ -22,8 +63,14 @@ program_names = {"c": "counter", "t": "timer",  "C": "clock", "a": "alarm"}
 user_inputs = ["c", "t", "C", "a", "q"]
 
 
-def user_input():
-    return "t"
+def list_input():
+    while True:
+        print("choose: ", end="")
+        choice = input()
+        if choice in user_inputs:
+            return choice
+        else:
+            print("Wrong choice")
 
 
 def main():
@@ -33,14 +80,13 @@ def main():
         print("function list: ")
 
         for element in program_names:
-            print("   '", element, "' for ", program_names[element])
-        print("   ' q ' to quit")
+            print("   '" + element + "' for", program_names[element])
+        print("   'q' to quit")
 
-        choice = user_input()
+        choice = list_input()
 
         if choice != "q":
             input_program[choice]()
-            break
         else:
             break
 
